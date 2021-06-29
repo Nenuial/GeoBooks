f02253dca3483e1ddf7f097a7f7b4f8c7133ecc8
Pascal Burkhard <pascal.burkhard@gmail.com>
2021-06-29 16:08:19 +0200

    Initial commit

---
+++ inst/GuideTM/_render.R
@@ -0,0 +1,24 @@
+quiet = "--quiet" %in% commandArgs(FALSE)
+formats = commandArgs(TRUE)
+
+src = (function() {
+  attr(body(sys.function()), 'srcfile')
+})()$filename
+if (is.null(src) || src == '') src = '.'
+owd = setwd(dirname(src))
+
+# provide default formats if necessary
+if (length(formats) == 0) formats = c(
+  'geovizr::gvz_bs4_book', 'geovizr::gvz_book'
+)
+# render the book to all formats unless they are specified via command-line args
+for (fmt in formats) {
+  cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
+  res = xfun::Rscript(c('-e', shQuote(cmd)))
+  if (res != 0) stop('Failed to compile the book to ', fmt)
+}
+
+unlink('*.log', expand = TRUE)
+unlink('*.ptc', expand = TRUE)
+
+setwd(owd)
